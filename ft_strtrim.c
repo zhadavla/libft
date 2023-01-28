33e197845be797802b24922e1444e64a4e7cd4dd
Vladislav Zhadan <vzhadan@c1r8s5.42prague.com>
2023-01-28 15:04:08 +0100

    More efficient implementation of ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,46 +6,11 @@
 /*   By: vzhadan <vzhadan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 19:59:05 by vzhadan           #+#    #+#             */
-/*   Updated: 2023/01/28 14:53:54 by vzhadan          ###   ########.fr       */
+/*   Updated: 2023/01/28 15:03:07 by vzhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	*cut_start(char const *s1, char const *set)
-{
-	while (*(s1))
-	{
-		if (!ft_strchr(set, *s1))
-			return ((char *)s1);
-		s1++;
-	}
-	return (NULL);
-}
-
-char	*cut_end(char *trimmed_from_beginning, char const *set)
-{
-	size_t	len;
-	size_t	i;
-	char	*str;
-
-	if (!trimmed_from_beginning)
-		return ("");
-	i = 0;
-	len = ft_strlen(trimmed_from_beginning);
-	while (i < len)
-	{
-		if (!ft_strchr(set, trimmed_from_beginning[len - i - 1]))
-			break ;
-		i++;
-	}
-	str = (char *)malloc(sizeof(char) * (len - i) + 1);
-	if (!str)
-		return (NULL);
-	ft_strlcpy(str, trimmed_from_beginning, len - i + 1);
-	return (str);
-}
-
 
 /*
  *	Allocates with malloc() and returns a copy of
@@ -54,9 +19,23 @@ char	*cut_end(char *trimmed_from_beginning, char const *set)
 */
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	if (!s1)
+	int		start;
+	int		end;
+	char	*str;
+
+	if (!s1 || !set)
 		return (NULL);
-	if (!set)
-		return (ft_strdup(s1));
-	return (cut_end(cut_start(s1, set), set));
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[start]) && start <= end)
+		start++;
+	if (start == end + 1)
+		return ("");
+	while (ft_strchr(set, s1[end]) && end >= 0)
+		end--;
+	str = (char *)malloc(sizeof(char) * (end - start + 1) + 1);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1 + start, end - start + 2);
+	return (str);
 }
